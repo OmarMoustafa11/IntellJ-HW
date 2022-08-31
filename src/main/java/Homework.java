bc37e6bbf01157e1c4f1cd8319713243df2f9f45
omarmoustafa11 <omaroj99@yahoo.com>
2022-08-31 11:42:17 -0400

    completed more work

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,5 +1,22 @@
 public class Homework {
     public static void main(String[] args) {
+        //JAVA OUTPUT
         System.out.println("This is my Homework");
+
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is so cool");
+
+        System.out.print("this line is going to merge with the next.");
+        System.out.print("i am the merging line");
+
+        //J
+
+
+
+
+        //
+
+
     }
 }
